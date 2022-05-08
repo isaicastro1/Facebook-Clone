50e8a09318f6550aca2d0fd782b6bf541ad28c3d
isaicastro1 <isaicstr@gmail.com>
2022-05-07 23:54:56 -0700

    Added html files for links

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navBarLinks = document.getElementsByClassName("links")[0];
+// const toggleButton = document.getElementsByClassName("toggle-button")[0];
+// const navBarLinks = document.getElementsByClassName("links")[0];
 
-// Menu Button Opens and Closes
-toggleButton.addEventListener("click", () => {
-  navBarLinks.classList.toggle("active");
-});
+// // Menu Button Opens and Closes
+// toggleButton.addEventListener("click", () => {
+//   navBarLinks.classList.toggle("active");
+// });
