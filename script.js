c692af219c21bf431f27677ea3c260c3ea02cf9c
isaicastro1 <isaicstr@gmail.com>
2022-06-11 13:36:49 -0700

    Changing layout to single page

---
+++ script.js
@@ -0,0 +1,7 @@
+// const toggleButton = document.getElementsByClassName("toggle-button")[0];
+// const navBarLinks = document.getElementsByClassName("links")[0];
+
+// // Menu Button Opens and Closes
+// toggleButton.addEventListener("click", () => {
+//   navBarLinks.classList.toggle("active");
+// });
