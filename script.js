082c3d5ec82f46c7f648993fda10e21fc6dcc44a
isaicastro1 <isaicstr@gmail.com>
2022-07-20 22:22:33 -0700

    Added fixed images instead of rendering them from website

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// const toggleButton = document.getElementsByClassName("toggle-button")[0];
-// const navBarLinks = document.getElementsByClassName("links")[0];
-
-// // Menu Button Opens and Closes
-// toggleButton.addEventListener("click", () => {
-//   navBarLinks.classList.toggle("active");
-// });
+function goto(){
+    document.location = "index2.html";
+}
